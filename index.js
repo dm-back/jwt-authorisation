4a38c7fe5ce3d3c7e4ae24c57e7310756c7be384
dm-back <dm-back@yandex.ru>
2022-01-15 20:46:02 +0300

    adding(resolve) cors politic

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const mongoose = require("mongoose");
 const authRouter = require("./authRouter");
 const app = express();
 const PORT = process.env.PORT || 5000;
+const cors = require('cors');
 
+app.use(cors());
 app.use(express.json());
 app.use("/auth", authRouter);
 
